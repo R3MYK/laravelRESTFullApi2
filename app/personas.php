f019f3ab4906bb5860c1334455701140fb985b3e
R3MYK <remyblunt_@hotmail.com>
2018-10-15 19:39:37 -0300

    Creación Modelo Personas y migración local
    Se ha creado el modelo personas y migración para la base de datos

---
+++ app/personas.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class personas extends Model{
+	protected $table="personas"; 
+	protected $primaryKey="id";
+	protected $fillable=array('nombre','apellido','rut','fecha'); 
+}
