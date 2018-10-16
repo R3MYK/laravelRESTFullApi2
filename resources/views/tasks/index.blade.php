eb364bc2f7a203c30b0c0d7bf11554f35c319860
R3MYK <remyblunt_@hotmail.com>
2018-10-15 23:10:50 -0300

    Creación de Vistas

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,10 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Personas</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    @foreach($personas as $personas)
+                        {{$personas->nombre}}<br>
+                    @endforeach
                 </div>
             </div>
         </div>
